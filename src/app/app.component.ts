9250baf6f4933168576adf43af8076138e3cf7de
KICK Sharath <kicksharath234@gmail.com>
2024-03-13 22:26:18 +0530

    Image Resize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Pica from 'pica';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'image-resizer';
+  
+  imageData: string = '';
+  resizedImage: string | ArrayBuffer | null = null;
+
+  constructor() { }
+
+  resizeImage() {
+    const img = new Image();
+    img.onload = () => {
+      const pica = new Pica();
+      const targetWidth = 1200; 
+      const targetHeight = 1200;
+  
+      const aspectRatio = img.width / img.height;
+  
+      let resizeWidth = targetWidth;
+      let resizeHeight = targetWidth / aspectRatio;
+  
+      if (resizeHeight > targetHeight) {
+        resizeWidth = targetHeight * aspectRatio;
+        resizeHeight = targetHeight;
+      }
+  
+      const canvas = document.createElement('canvas');
+      canvas.width = resizeWidth;
+      canvas.height = resizeHeight;
+  
+      pica.resize(img, canvas)
+        .then((result: HTMLCanvasElement) => {
+          const dataURL = result.toDataURL('image/jpeg', 1);
+          this.resizedImage = dataURL;
+        })
+        .catch((error: any) => {
+          console.error('Error resizing image:', error);
+        });
+    };
+  
+    img.src = this.imageData;
+  }
+  
+  
+  
 }
